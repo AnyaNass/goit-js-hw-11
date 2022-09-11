33e03c665576127d86f55b7232131525938102e6
AnyaNass <98224289+AnyaNass@users.noreply.github.com>
2022-09-11 17:19:27 +0000

    Deploying to gh-pages from @ AnyaNass/goit-js-hw-11@606e2911c251f966accd1876e5a8abad0931d332 🚀

---
+++ index.37b89f66.js
@@ -0,0 +1,2 @@
+var ref=document.querySelector(".header").getBoundingClientRect(),pageHeaderHeight=ref.height;document.body.style.paddingTop="".concat(pageHeaderHeight,"px");
+//# sourceMappingURL=index.37b89f66.js.map
