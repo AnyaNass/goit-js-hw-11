44ffbbfdfd0f144ad4704e252583f70b08a51e6f
Anna <98224289+AnyaNass@users.noreply.github.com>
2022-09-12 15:42:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,13 +17,13 @@ loadMoreBtn.addEventListener('click', onloadMoreBtnClick);
 function onSubmit(e) {
 	e.preventDefault();
 
-	// searchQueryInput = e.target.elements.searchQuery.value.trim();
+	searchQueryInput = e.target.elements.searchQuery.value.trim();
 
 	page = 1;
 	gallery.innerHTML = '';
 	theEndOfCollection.classList.add('is-hidden')
 
-	if (e.target.elements.searchQuery.value.trim() === '') {
+	if (searchQueryInput === '') {
 		return;
 	}
 
